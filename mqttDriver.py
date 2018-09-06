420b6a3787131cc8b415a0c31c5d26c944adb05c
sfalcona <sfalcona@itba.edu.ar>
2018-09-06 11:58:12 -0300

    Cambio typo, pruebo commit desde sublime

--- mqttDriver.py
+++ mqttDriver.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt
 
 
 def setClient(self,serverIp,serverPort=1883,userName = 'RPi', messageCallback = None):
-	'''Instancia de cliento Mosquitto'''
+	'''Instancia de cliente Mosquitto'''
 	myClient = mqtt.Client()
 	myClient.username_pw_set('RPi')
 	myClient.on_message = messageCallback
