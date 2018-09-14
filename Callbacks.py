8a7e7d46e46bffd1406e06f60534f7be86ac40b6
Bernardo Michel <bernardo.michel@avature.net>
2018-09-13 21:33:09 -0300

    Agrego archivo nuevo, pocos avances

---
+++ Callbacks.py
@@ -0,0 +1,13 @@
+import mqttDriver
+import json
+
+
+def idle2EsperandoOtroObjeto(room, obj):
+	'''Tengo que generar la acción en el room y obj que tienen como párametro
+	publicar en un tópico mediante mqtt 
+	Responder'''
+
+
+def idle2EsperandoAccion(room, obj):
+	''' Tengo que obtener el estado en el que está el objeto en el room ingresado
+		y enviar el estado obtenido'''
