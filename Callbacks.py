d39f355610c07f518825b0c38f7c36c6e980a715
bernardomichel <bmichel@itba.edu.ar>
2018-09-19 20:28:22 -0300

    Emprolijo la idea, sigue todo verde, falta definir "esperandoRespuesta" y "estado" y quien cambia esas variables

--- Callbacks.py
+++ Callbacks.py
@@ -22,20 +22,23 @@ def idle2EsperandoOtroObjeto(room, obj, accion):
 def idle2EsperandoAccion(room, obj):
 	''' Tengo que obtener el estado en el que está el objeto en el room ingresado
 	y enviar el estado obtenido'''
-	cliente.pusblish([room, obj],"estado")
+	cliente.pusblish(room + "/" + obj,"estado")
 	#  Una vez que hago un publish del estado, tengo que recibir la respuesta del mismo.
 	#esta respuesta la voy a escuchar con un suscribe.
 	
-	if ultimoMensaje == 'on':
-		#Puedo hacer que espere a la respuesta, si no se conectó bien, tiro erro de una
-	else
-		#fullfilmentText de error. Se podría hacer que intente de nuevo como para que sea más robusto
-	#publicar el tópico por mqtt
-	#nose como hacer para preguntar el estado
-	#ver la respuesta que recibo del mqtt driver
-	#genero una respuesta para que se mande al servidor response.json
-		# {
-		#	"fullfilmentText" : "la" . "obj" . "de " . "room" . "se encuentra " . "estado obtenido"
-		# }
+	#Espero la respuesta, esto puede ser un futuro callback
+	if cliente.suscribe(room + "/" + obj) == MQTT_ERR_SUCCESS:
+		while !esperandoRespuesta: #esperandoRespuesta tiene que modificarse cuando la ESP32 genera la respuesta con el estado del tópico que se publicó
+			if estado: #Tiene que guardar la respuesta en esta variable.
+				data = {"fullfilmentText": "Sí"}
+			else
+				data = {"fullfilmentText": "No"}
+    		#Para salir de este while, tengo que poner un timeout
+	else #fullfilmentText de error. Se podría hacer que intente de nuevo como para que sea más robusto
+		data = {"fullfilmentText" : "Ocurrió un error en la conección con Mi Casa De Lejos"}
+
+	#Guardo la respuesta en un archivo
+	with open("response.json", "w") as outfile:
+    	simplejson.dump(data, outfile)
 
 def esperandoObjeto2EsperandoAccion(room, obj):
